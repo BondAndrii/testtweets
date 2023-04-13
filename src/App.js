f7449d9d63ac31219f317303c7abaea9e9e7c42e
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-04-13 16:37:17 +0300

    Commit of change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hjghg
+          Change via pull-request
         </a>
       </header>
     </div>
