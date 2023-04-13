a11958f06d67bfb8eb53b3eaa4871eb22e156c0d
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-04-13 17:17:12 +0300

    del automat deploy add change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Change via pull-request #2
+          Change via pull-request #3
         </a>
       </header>
     </div>
